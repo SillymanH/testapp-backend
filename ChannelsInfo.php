c9403a41828593fafdb8d55eca99e1f23c38bdd9
Sleiman Hasan <sleiman.hasan@foo-solutions.com>
2020-01-06 01:03:30 +0200

    Changing ChannelInfo to OOP

--- ChannelsInfo.php
+++ ChannelsInfo.php
@@ -8,13 +8,36 @@ class ChannelsInfo {
 
     private $db_table = "channels";
 
-    public function __construct(){
+    //Channel Properties
+    private $userId;
+    private $channelId;
+    private $videoId;
+    private $channelURL;
+    private $subscribersNumber;
+    private $dateCreated;
+
+    //Channel Table Attributes
+//    private $channelIdAttribute = "channel_id";
+//    private $userIdAttribute = "user_id";
+//    private $channelURLAttribute = "channel_url";
+//    private $subscribersNumberAttribute = "channel_subscribers_number";
+//    private $dateCreatedAttribute = "date_created";
+    //TODO: Add a new attribute $videoIdAttribute
+
+    public function __construct($userId, $channelId, $videoId, $channelURL, $subscribersNumber, $dateCreated){
+
+        $this->userId = $userId;
+        $this->channelId = $channelId;
+        $this->videoId = $videoId;
+        $this->channelURL = $channelURL;
+        $this->subscribersNumber = $subscribersNumber;
+        $this->dateCreated = $dateCreated;
         $this->db = new DbConnect();
     }
 
-    public function getChannelInfo($userId, $channelId){
+    public function getChannelInfo($channelId){
 
-        $query = "select channel_url, channel_subscribers_number, date_created  from ".$this->db_table." where user_id = $userId AND channel_id = $channelId";
+        $query = "select *  from ".$this->db_table." where channel_id = $channelId";
 
         $result = mysqli_query($this->db->getDb(), $query);
 
@@ -24,7 +47,7 @@ class ChannelsInfo {
 
             foreach ($result as $row) {
 
-                  array_push($output, $row);
+                array_push($output, $row);
             }
             $json['success'] = 1;
 
@@ -37,6 +60,32 @@ class ChannelsInfo {
         $json['info'] = $output;
         return $json;
     }
+
+//    public function getChannelInfo($userId, $channelId){
+//
+//        $query = "select channel_url, channel_subscribers_number, date_created  from ".$this->db_table." where user_id = $userId AND channel_id = $channelId";
+//
+//        $result = mysqli_query($this->db->getDb(), $query);
+//
+//        $output = array();
+//
+//        if(mysqli_num_rows($result) > 0){
+//
+//            foreach ($result as $row) {
+//
+//                  array_push($output, $row);
+//            }
+//            $json['success'] = 1;
+//
+//            mysqli_close($this->db->getDb());
+//        }else {
+//
+//            $output = 'Failed to get channel info';
+//            $json['success'] = 0;
+//        }
+//        $json['info'] = $output;
+//        return $json;
+//    }
 }
 
 ?>
